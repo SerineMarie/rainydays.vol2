9bfdf0e48f64dbfcb6d0b023c59e8ca21eca08c7
SerineMarie <71319320+SerineMarie@users.noreply.github.com>
2021-04-17 17:45:23 +0200

    edited http to https

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const url = "http://serinetj.com/wp-json/wc/store/products";
+const url = "https://serinetj.com/wp-json/wc/store/products";
 const productsContainer = document.querySelector (".productsContainer");
 
 async function getProducts (){
