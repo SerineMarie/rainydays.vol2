7124bd186d75eee12e0c94c3819971f97db621fd
SerineMarie <71319320+SerineMarie@users.noreply.github.com>
2021-04-17 17:09:20 +0200

    added styles to new js files

--- js/api.js
+++ js/api.js
@@ -19,19 +19,18 @@ getProducts();
 function createHTML(product){
     product.forEach(function(product){
         productsContainer.innerHTML += `
-                            
-                                <h1> Women</h1> 
+                                
                                 <div class="products">                       
                                     <a href="productdetail.html?id=${product.id}">
-
-                                        <h2>
-                                            <p>New In</p>                
-                                            <p> ${product.name}</p>
-                                            <p> ${product.id}</p>
-                                            <img class="img" src="${product.images[0].src}" alt="${product.alt}">
-                                            <p>£${product.prices.price}</p>
-                                        </h2>
+                                        <div class="ball">   
+                                            <p class="category">${product.categories[0].name}</p> 
+                                        </div>
+            
+                                        <img class="img" src="${product.images[0].src}" alt="${product.alt}">
+                                        <p class="name-price"> ${product.name}</p>
+                                            <p class="name-price">£${product.prices.price}</p>
                                     </a>
+                                    <div class="line"></div>
                                 </div>`;
     })
 }
